f4dd0bee343335472d32eff3340f7752ed678ac8
Akash-tiwari05 <myselfakashtiwari05@gmail.com>
2026-03-07 15:12:02 +0530

    Added some quarry's in repository

--- ride-review-service/src/main/java/com/akash/ride_review_service/repositories/DriverRepositories.java
+++ ride-review-service/src/main/java/com/akash/ride_review_service/repositories/DriverRepositories.java
@@ -1,14 +1,15 @@
 package com.akash.ride_review_service.repositories;
 
 import com.akash.ride_review_service.model.Booking;
+import com.akash.ride_review_service.model.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+import java.util.Optional;
 
 @Repository
-public interface BookingRepositories extends JpaRepository<Booking,Long> {
+public interface DriverRepositories extends JpaRepository<Driver,Long> {
 
-    List<Booking> findAllByDriverId(Long driverId);
+    Optional<Driver> findByIdAndLicenseNumber(Long id, String licenceNumber);
 }
